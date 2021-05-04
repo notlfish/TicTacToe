43d69c4f0300dcbd0e8188aabae5a594f81d124e
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-05-04 17:18:24 -0300

    Refactor spec/main_spec.rb

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,23 +2,24 @@ require 'spec_helper'
 require_relative '../bin/main'
 require_relative '../lib/board'
 
+map_output = <<~OUT
+  +---+---+---+
+  | 1 | 2 | 3 |
+  +---+---+---+
+  | 4 | 5 | 6 |
+  +---+---+---+
+  | 7 | 8 | 9 |
+  +---+---+---+
+OUT
+
 RSpec.describe 'Tests for TicTacToeUI' do
   describe 'Test for #display_board' do
     it 'Prints example board to stdout' do
-      message = <<~MSG
-        +---+---+---+
-        | 1 | 2 | 3 |
-        +---+---+---+
-        | 4 | 5 | 6 |
-        +---+---+---+
-        | 7 | 8 | 9 |
-        +---+---+---+
-      MSG
       expect do
         ui = TicTacToeUI.new
         mock = Board.new(:map)
         ui.display_board(mock)
-      end.to output(message).to_stdout
+      end.to output(map_output).to_stdout
     end
   end
 end

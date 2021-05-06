df938b71d9e0eb4b04f953eeccf8a2fe51e49ef0
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-05-06 13:23:15 -0300

    Add tests to TicTacToeLogic

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -2,17 +2,18 @@ require 'spec_helper'
 require_relative '../bin/main'
 require_relative '../lib/game_logic'
 
-def test_game
-  test_game = TicTacToeLogic.new
-  test_game.play(0, 'X')
-  test_game.play(4, 'X')
-  test_game.play(1, 'O')
-  test_game.play(8, 'O')
-  test_game
-end
-tokens = %w[X O]
-
 RSpec.describe 'Tests for TicTacToeLogic' do
+  let(:test_game) do
+    test_game = TicTacToeLogic.new
+    test_game.play(0, 'X')
+    test_game.play(4, 'X')
+    test_game.play(1, 'O')
+    test_game.play(8, 'O')
+    test_game
+  end
+
+  let(:tokens) { %w[X O] }
+
   describe 'Tests for #valid_move?' do
     it 'Is true for unoccupied cell' do
       expect(test_game.valid_move?(2)).to be(true)
@@ -36,11 +37,13 @@ RSpec.describe 'Tests for TicTacToeLogic' do
       expect(test_game.turn).to eq(4)
     end
 
+    it 'Checks that uplayed cell is empty' do
+      expect(test_game.board.row(2)[0]).to eq(' ')
+    end
+
     it 'Updates board cell when valid move' do
-      test = test_game
-      expect(test.board.row(2)[0]).to eq(' ')
-      test.play(6, 'X')
-      expect(test.board.row(2)[0]).to eq('X')
+      test_game.play(6, 'X')
+      expect(test_game.board.row(2)[0]).to eq('X')
     end
 
     it 'Returns true when play on a free cell' do
@@ -53,12 +56,21 @@ RSpec.describe 'Tests for TicTacToeLogic' do
   end
 
   describe 'Test for #winner' do
+    let(:win_row) do
+      win_row = TicTacToeLogic.new
+      3.times { |i| win_row.play(i, 'X') }
+      win_row
+    end
+
+    let(:win_col) do
+      win_col = TicTacToeLogic.new
+      3.times { |i| win_col.play(3 * i, 'X') }
+      win_col
+    end
+
     it 'Returs false for ongoing game' do
       expect(test_game.winner(tokens)).to be(false)
     end
-
-    win_row = TicTacToeLogic.new
-    3.times { |i| win_row.play(i, 'X') }
 
     it 'Returns winner token when X wins' do
       expect(win_row.winner(tokens)).to eq('X')
@@ -68,17 +80,17 @@ RSpec.describe 'Tests for TicTacToeLogic' do
       expect(win_row.winner(tokens)).to eq('X')
     end
 
-    win_col = TicTacToeLogic.new
-    3.times { |i| win_col.play(3 * i, 'X') }
-
     it 'Returns winner token when win on col' do
       expect(win_col.winner(tokens)).to eq('X')
     end
 
-    win_diag = TicTacToeLogic.new
-    win_diag.play(2, 'O')
-    win_diag.play(4, 'O')
-    win_diag.play(6, 'O')
+    let(:win_diag) do
+      win_diag = TicTacToeLogic.new
+      win_diag.play(2, 'O')
+      win_diag.play(4, 'O')
+      win_diag.play(6, 'O')
+      win_diag
+    end
 
     it 'Returns winner token when win on diagonal' do
       expect(win_diag.winner(tokens)).to eq('O')
@@ -88,23 +100,31 @@ RSpec.describe 'Tests for TicTacToeLogic' do
       expect(win_diag.winner(tokens)).to eq('O')
     end
 
-    it 'Returns :tie for tied game' do
+    let(:tied_game) do
       tied_game = test_game
       tied_game.play(2, 'X')
       tied_game.play(3, 'O')
       tied_game.play(5, 'O')
       tied_game.play(6, 'O')
       tied_game.play(7, 'X')
+      tied_game
+    end
+
+    it 'Returns :tie for tied game' do
       expect(tied_game.winner(tokens)).to eq(:tie)
     end
 
-    it 'Returns winner token when win on last move' do
+    let(:won_last) do
       won_last = test_game
       won_last.play(2, 'X')
       won_last.play(5, 'O')
       won_last.play(6, 'X')
       won_last.play(7, 'O')
       won_last.play(3, 'X')
+      won_last
+    end
+
+    it 'Returns winner token when win on last move' do
       expect(won_last.winner(tokens)).to eq('X')
     end
   end
